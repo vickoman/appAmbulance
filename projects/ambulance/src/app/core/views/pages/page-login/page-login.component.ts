0b742e028598762b4f7cd57cb2aa78bba4ec2ff3
vickoman <vicmandlagasca@gmail.com>
2021-06-21 17:38:23 -0500

    Improve the code to get input from loginForm

--- projects/ambulance/src/app/core/views/pages/page-login/page-login.component.ts
+++ projects/ambulance/src/app/core/views/pages/page-login/page-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { User } from 'projects/ambulance/src/app/users/domain/user.interface';
 
 @Component({
   selector: 'amb-page-login',
@@ -9,31 +10,20 @@ export class PageLoginComponent implements OnInit {
   @ViewChild('fieldEmail') fieldEmail: ElementRef | undefined;
   @ViewChild('fieldPassword') fieldPassword: ElementRef | undefined;
 
-  userEmail = '';
-  userPassword = '';
+  user: Partial<User> = {};
 
   constructor() {}
 
-  set email(e: any) {
-    this.userEmail = e.target.value.trim();
-  }
-
-  set password(e: any) {
-    this.userPassword = e.target.value.trim();
-  }
-
-  get email() {
-    return this.userEmail.toLowerCase();
-  }
-
-  get password() {
-    return this.userPassword.toLowerCase();
+  getInputsFromUser(): Partial<User> {
+    return {
+      email: this.fieldEmail?.nativeElement.value,
+      password: this.fieldPassword?.nativeElement.value,
+    };
   }
 
   buttonHandler() {
     console.log('Evento handler click');
-    console.log(this.fieldEmail?.nativeElement.value);
-    console.log(this.fieldPassword?.nativeElement.value);
+    console.log(this.getInputsFromUser());
   }
 
   ngOnInit(): void {}
