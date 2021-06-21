e9451e5b55081f5cd80142fd00c33cad2bfba4a7
vickoman <vicmandlagasca@gmail.com>
2021-06-21 16:02:53 -0500

    List from variables

--- projects/ambulance/src/app/own.component.ts
+++ projects/ambulance/src/app/own.component.ts
@@ -1,7 +1,40 @@
 import { Component } from '@angular/core';
 
+interface User {
+    name: string
+    lastName: string
+    age: number
+}
 @Component({
     selector: "app-own",
-    template: "<h3>My own component</h3>",
+    templateUrl: './own.component.html',
+    styleUrls: ['./own.component.css']
 })
-export class OwnComponent {}
+export class OwnComponent {
+    listUsers: Array<User> = [
+        {
+            name: "Victor",
+            lastName: "Diaz",
+            age: 35
+        },
+        {
+            name: "Oscar",
+            lastName: "Perez",
+            age: 44
+        },
+        {
+            name: "Ericka",
+            lastName: "Peralta",
+            age: 30
+        },
+        {
+            name: "Priscila",
+            lastName: "Aviles",
+            age: 20
+        }
+    ];
+
+    constructor(){
+        this.listUsers.map(user => console.log(user.name));
+    }
+}
